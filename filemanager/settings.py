2ccbe69c4e019eec8a221ad89c50a94cbcb3d899
Black Chaos <97971337+blackchaos1@users.noreply.github.com>
2024-06-04 12:57:42 +0300

    Update settings.py

--- filemanager/settings.py
+++ filemanager/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 CSRF_TRUSTED_ORIGINS = [
-    '*',  # Replace with your actual Ngrok URL
+    'https://127.0.0.1',  # Replace with your actual Ngrok URL
 ]
 
 # Application definition
